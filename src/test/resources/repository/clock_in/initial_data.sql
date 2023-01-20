bbcc1c4b79e67fd05a4b820fde31e7645d187525
Luis Moral Guerrero <luis.moral@gmail.com>
2023-01-20 17:55:42 +0100

    Refactoring.

--- src/test/resources/repository/clock_in/initial_data.sql
+++ src/test/resources/repository/clock_in/initial_data.sql
@@ -1,7 +1,7 @@
 -- alert
-INSERT INTO alert (business_id, expression, message) VALUES ('businessId1', '#clockIn.timeWorked() > T(java.util.concurrent.TimeUnit).HOURS.toMillis(10)', 'message1');
-INSERT INTO alert (business_id, expression, message) VALUES ('businessId1', '#clockIn.timeWorked() > T(java.util.concurrent.TimeUnit).HOURS.toMillis(10)', 'message2');
-INSERT INTO alert (business_id, expression, message) VALUES ('businessId1', '#clockIn.timeWorked() > T(java.util.concurrent.TimeUnit).HOURS.toMillis(10)', 'message2');
+INSERT INTO alert (business_id, expression, message) VALUES ('businessId1', '#clockIn.timeWorked() > T(java.util.concurrent.TimeUnit).HOURS.toMillis(10)', 'Missing clock ins');
+INSERT INTO alert (business_id, expression, message) VALUES ('businessId1', '#clockIn.timeWorked() > T(java.util.concurrent.TimeUnit).HOURS.toMillis(10)', 'Maximum work hours exceeded');
+INSERT INTO alert (business_id, expression, message) VALUES ('businessId1', '(#clockIn.dayOfWeek().isPresent()) and ((#clockIn.dayOfWeek().get() >= T(java.time.DayOfWeek).MONDAY && #clockIn.dayOfWeek().get() <= T(java.time.DayOfWeek).THURSDAY && #clockIn.firstRecordHourOfDay() >= 8) or (#clockIn.dayOfWeek().get() == T(java.time.DayOfWeek).FRIDAY &&#clockIn.firstRecordHourOfDay() >= 7))', 'Invalid first clock in time');
 
 -- clock_in
 INSERT INTO clock_in (business_id, employee_id, service_id, date) VALUES ('businessId1', 'employeeId1', 'serviceId1', 0);
